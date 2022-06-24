7e2c8b2dd6d09b825ee5ddc921c629637220516b
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-06-24 20:38:50 +0100

    Added second test and modified first

--- workshop/index.js
+++ workshop/index.js
@@ -1,5 +1,14 @@
-const name = "pikachu";
-
 function makeUrl(name) {
   return "https://pokeapi.co/api/v2/" + name;
+}
+
+function searchParamsToObject(string) {
+  let words = string.split("&");
+  let obj = {};
+  for (let i = 0; i < words.length; i++) {
+    const keys = words[i].split("=")[0];
+    const values = words[i].split("=")[1];
+    obj[keys] = values;
+  }
+  return obj;
 }
--- workshop/index.test.js
+++ workshop/index.test.js
@@ -1,9 +1,27 @@
 test(`Test makeUrl`, () => {
   const expected = "https://pokeapi.co/api/v2/pikachu";
-  const actual = "https://pokeapi.co/api/v2/pikachu";
-  if (expected === actual) {
-    console.log(`No error`);
-  } else {
-    console.error(`Expected ${expected} but received ${actual}`);
-  }
+  const actual = makeUrl("pikachu");
+  equal(actual, expected);
+});
+
+test(`Test Objects have same name value`, () => {
+  const expected = { name: "oliver", email: "hello@oliverjam.es" }.name;
+  const actual = searchParamsToObject(
+    "name=oliver&email=hello@oliverjam.es"
+  ).name;
+  equal(actual, expected);
+});
+
+test(`Check objects are not exactly the same`, () => {
+  const expected = { name: "oliver", email: "hello@oliverjam.es" };
+  const actual = searchParamsToObject(`name=oliver&email=hello@oliverjam.es"`);
+  notEqual(actual, expected);
+});
+
+test(`Objects contain same email`, () => {
+  const expected = { name: "oliver", email: "hello@oliverjam.es" }.email;
+  const actual = searchParamsToObject(
+    `name=oliver&email=hello@oliverjam.es`
+  ).email;
+  equal(actual, expected);
 });
